e953423548da2bde92c40e22ebd3101866ebf9c1
hanggaoh <hanggaoh@gmail.com>
2024-10-04 00:39:45 +1300

    bug for cd to the folder of python script

--- mvFileCron.sh
+++ mvFileCron.sh
@@ -3,6 +3,7 @@
 # Navigate to the directory where mvVideo.py is located
 # Change this to the actual directory
 # Run the Python script with the first destination
+cd /home/pi/IDRecognizer
 python3 ./mvVideo.py /media/pi/ssd /media/pi/hdd1/
 
 # Check if the script was successful
