2cd411149cf5687e8cf6f93e639cb66e1b8bc068
hanggaoh <hanggaoh@gmail.com>
2024-10-04 00:23:39 +1300

    alternative destination for mvfiles cron

--- mvFileCron.sh
+++ mvFileCron.sh
@@ -2,6 +2,12 @@
 
 # Navigate to the directory where mvVideo.py is located
 # Change this to the actual directory
-cd /home/pi/IDRecognizer
-# Run the Python script
+# Run the Python script with the first destination
 python3 ./mvVideo.py /media/pi/ssd /media/pi/hdd1/
+
+# Check if the script was successful
+if [ $? -ne 0 ]; then
+    echo "Primary destination failed, attempting alternative destination..."
+    # Run the Python script with an alternative destination
+    python3 ./mvVideo.py /media/pi/ssd /media/pi/HDD/
+fi
