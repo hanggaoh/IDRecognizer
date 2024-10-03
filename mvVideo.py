077b7928b48b7de256e3727dd1c7c6240b95d679
hanggaoh <hanggaoh@gmail.com>
2024-10-03 13:05:14 +1300

    use constant

--- mvVideo.py
+++ mvVideo.py
@@ -4,6 +4,8 @@ import sys
 
 from matcher.matcher import match_and_format
 from utils.file import check_file_in_dir
+from utils.constants import video_extensions
+from utils.constants import patterns
 import shutil
 
 from logger_setup import get_logger
@@ -11,28 +13,7 @@ from logger_setup import get_logger
 logger = get_logger()
 
 # Extended list of video file extensions
-video_extensions = {
-    '.mp4', '.avi', '.mkv', '.mov', '.flv', '.wmv', '.m4v', '.mpg', '.mpeg', 
-    '.3gp', '.webm', '.ogv', '.vob', '.rm', '.rmvb', '.m2ts', '.mts', '.ts', 
-    '.mxf', '.divx', '.f4v', '.asf', '.amv', '.svi', '.3g2', '.m2v', '.mpe',
-    '.mpv', '.m1v', '.m4p', '.m4b'
-}
 
-patterns = {
-    r'(heyzo).*(\d{4})': r'\1-\2',
-    r'(?i)(fc2).*(ppv).*(\d{7}|\b\d{6}\b)': r'\1-\2-\3',
-    r'(?i)(\d{6})[-_](\d{3}).*(carib|1pon)': r'\3-\1-\2',
-    r'(?i)(carib|1pon).*(\d{6})[-_](\d{3}).*': r'\1-\2-\3',
-    r'(\d{6})[-_](\d{3})': r'\1-\2',
-    r'([a-zA-Z]{4})[0]*(\d{3,4})': r'\1-\2',
-    r'([nN]\d{4})': r'\1',
-    r'([a-zA-Z]{4,6})[-_0](\d{2,4})': r'\1-\2',
-    r'([a-zA-Z0-9]{2,8})[-_0](\d{2,4})': r'\1-\2',
-    r'([a-zA-Z]{3,4})(\d{2,4})': r'\1-\2',
-    r"([a-z]{2,5})-(\d{2,3})_(\d{1,2})": lambda m: f"{m.group(1).capitalize()}-{m.group(2).zfill(3)}_{m.group(3).zfill(2).upper()}",
-    r"([a-z]{2,5})-(\d{2,3})[-_]?cd([\d]{1,2})": lambda m: f"{m.group(1).capitalize()}-{m.group(2).zfill(3)}_{m.group(3).zfill(2).upper()}",
-    r"([a-z]{2,5})-(\d{2,3})([A-E])": lambda m: f"{m.group(1).capitalize()}-{m.group(2).zfill(3)}_{m.group(3)}"
-    }
 
 def find_videos(directory):
     # Walk through directory and subdirectories
