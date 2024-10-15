fb2280cad324431b7d9d4ebc60a653bbeb315298
hanggaoh <hanggaoh@gmail.com>
2024-10-15 15:43:27 +1300

    change the target drive

--- sh/mvFileCron.sh
+++ sh/mvFileCron.sh
@@ -4,11 +4,11 @@
 # Change this to the actual directory
 # Run the Python script with the first destination
 cd /home/pi/IDRecognizer
-python3 ./mvVideo.py /home/pi/smbshare
+sudo python3 ./mvVideo.py /media/pi/ssd /media/pi/sdb1/
 
 # Check if the script was successful
 if [ $? -ne 0 ]; then
     echo "Primary destination failed, attempting alternative destination..."
     # Run the Python script with an alternative destination
-    python3 ./mvVideo.py /media/pi/ssd /media/pi/sdc1/
+    sudo python3 ./mvVideo.py /media/pi/ssd /media/pi/sdc1/
 fi
