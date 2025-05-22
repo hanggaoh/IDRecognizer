d10218d6e09f853d43fb3eccb0f99e0a7081505e
hanggaoh <hanggaoh@gmail.com>
2025-05-22 22:33:21 +1200

    setup cronMV for root

--- sh/setupCronMV.sh
+++ sh/setupCronMV.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Get the current working directory
-currentPWD=$(pwd)
+script_path="$(realpath "$0")"
+parent_folder="$(dirname "$script_path")"
+workingDirectory="$(dirname "$(dirname "$(realpath "$0")")")"
+log_folder="$workingDirectory/logs"
 
 # Define the log file path
-logFile="$currentPWD/logs/mv_cron.log"
+logFile="$log_folder/mv_cron.log"
 
 # Create the logs directory if it doesn't exist
-mkdir -p "$currentPWD/logs"
+mkdir -p "$log_folder"
 
 # Check if cron is installed, and install if not
 if ! command -v cron &> /dev/null; then
@@ -18,22 +21,15 @@ if ! command -v cron &> /dev/null; then
     sudo systemctl start cron
 fi
 
-# Check if there is a crontab for the pi user
-if ! crontab -u pi -l &>/dev/null; then
-    # Create an empty crontab for pi
-    crontab -u pi -l > temp_cron
+# Check if the cron job already exists for root
+sudo crontab -l -u root 2>/dev/null > temp_cron || true
+if ! grep -q "$workingDirectory/sh/mvFileCron.sh" temp_cron; then
+    # Add the cron job for root
+    echo "0 */4 * * * $workingDirectory/sh/mvFileCron.sh >> $logFile 2>&1" >> temp_cron
+    sudo crontab -u root temp_cron
+    echo "Cron job added for root: $workingDirectory/sh/mvFileCron.sh every 4 hours"
 else
-    crontab -u pi -l > temp_cron
-fi
-
-# Check if the cron job already exists
-if ! grep -q "$currentPWD/sh/mvFileCron.sh" temp_cron; then
-    # Add the cron job for pi
-    echo "0 0 * * * $currentPWD/sh/mvFileCron.sh >> $logFile 2>&1" >> temp_cron
-    crontab -u pi temp_cron
-    echo "Cron job added for user pi: $currentPWD/sh/mvFileCron.sh every 24 hours"
-else
-    echo "Cron job already exists for user pi."
+    echo "Cron job already exists for root."
 fi
 
 # Clean up
